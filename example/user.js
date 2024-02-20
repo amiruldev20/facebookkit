d9c157aeaf156a5b5a618dee134480905b0ffe54
Amirul Dev <73012169+amiruldev20@users.noreply.github.com>
2024-02-20 04:40:15 +0000

    add example

---
+++ example/user.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+const login = require("facebookkit");
+
+login({appState: JSON.parse(fs.readFileSync('session.json', 'utf8'))}, (err, api) => {
+    if(err) return console.error(err);
+
+    api.setOptions({listenEvents: true});
+
+    var stopListening = api.listenMqtt((err, event) => {
+        if(err) return console.error(err);
+
+        api.markAsRead(event.threadID, (err) => {
+            if(err) console.error(err);
+        });
+
+        switch(event.type) {
+            case "message":
+                if(event.body === '/stop') {
+                    api.sendMessage("Goodbye…", event.threadID);
+                    return stopListening();
+                }
+                api.sendMessage("TEST BOT: " + event.body, event.threadID);
+                break;
+            case "event":
+                console.log(event);
+                break;
+        }
+    });
+});
